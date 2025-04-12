7f725a3b1ae30bdd0a4b9422a4a76f3244074dd9
RaedFelfelInstrutcor <Raaed.Felfel.Instructor@gmail.com>
2025-04-12 13:18:34 +0300

    complete the views of home page.

--- Models/Home/HomeViewModel.cs
+++ Models/Home/HomeViewModel.cs
@@ -5,8 +5,11 @@
         public IEnumerable<CarouselItemViewModel> CarouselItems { get; set; }
         public IEnumerable<ProductViewModel> FeaturedProducts { get; set; }
         public IEnumerable<ProductViewModel> NewArrivals { get; set; }
-
         public SpecialOfferViewModel? SpecialOffer { get; set; }
+        public IEnumerable<CategoryViewModel> Categories { get; set; }
+        public IEnumerable<TestimonialViewModel> Testimonials { get; set; }
+        public CallToActionViewModel CallToAction { get; set; }
+
 
     }
 }
